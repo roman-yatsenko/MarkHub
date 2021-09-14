fdd0e1244db4c058c0c22659398f4b6db6ce4b7d
roman-yatsenko <yatsenkoroma@gmail.com>
2021-09-14 06:35:34 +0300

    Add github token for current user

--- markhub/views.py
+++ markhub/views.py
@@ -3,3 +3,12 @@ from django.views.generic import TemplateView
 
 class Home(TemplateView):
     template_name = 'home.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        social_account = self.request.user.socialaccount_set
+        if social_account.exists() and social_account.first().provider == 'github':
+            social_login = social_account.first().socialtoken_set
+            if social_login.exists():
+                print(social_login.first().token)
+        return context
