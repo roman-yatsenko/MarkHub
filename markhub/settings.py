589a9ad76245020a547a12fa85d7f13a24ae9ba5
roman-yatsenko <yatsenkoroma@gmail.com>
2021-09-21 06:50:20 +0300

    Add django-environ

--- markhub/settings.py
+++ markhub/settings.py
@@ -13,18 +13,29 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 import os
 from pathlib import Path
 
+import environ
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+# Take environment variables from .env file
+env = environ.Env(
+    # set casting, default value
+    DEBUG=(bool, False)
+)
+
+environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-=+4k3pxi=)y_7#x7(ujrm803@b!pgosh3=y16e%-2py5#-z&ol'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# False if not in os.environ because of casting above
+DEBUG = env('DEBUG')
 
 ALLOWED_HOSTS = []
 
--- markhub/views.py
+++ markhub/views.py
@@ -8,10 +8,12 @@ class Home(TemplateView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        social_account = self.request.user.socialaccount_set
-        if social_account.exists() and social_account.first().provider == 'github':
-            social_login = social_account.first().socialtoken_set
-            if social_login.exists():
-                g = Github(social_login.first().token)
-                context['repos'] = [repo.name for repo in g.get_user().get_repos()]
+        user = self.request.user
+        if user.is_authenticated:
+            social_account = user.socialaccount_set
+            if social_account.exists() and social_account.first().provider == 'github':
+                social_login = social_account.first().socialtoken_set
+                if social_login.exists():
+                    g = Github(social_login.first().token)
+                    context['repos'] = [repo.name for repo in g.get_user().get_repos()]
         return context
