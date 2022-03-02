12ae96eb632fa9bad85587fc6a2e90403e7f69e1
roman-yatsenko <yatsenkoroma@gmail.com>
2022-03-02 15:57:12 +0200

    Update 02.03 15:57

--- markhub/forms.py
+++ markhub/forms.py
@@ -1,12 +1,14 @@
 from typing import Optional, Mapping, Union, Any, Type
 from django import forms
 
+from martor.fields import MartorFormField
+
 
 class NewFileForm(forms.Form):
     """ New File Form"""
 
     filename = forms.CharField(max_length=256, label='File name', required=True)
-    content = forms.CharField(widget=forms.Textarea, label='File content')
+    content = MartorFormField(label='File content')
 
 
 class UpdateFileForm(forms.Form):
@@ -14,7 +16,7 @@ class UpdateFileForm(forms.Form):
 
     filename = forms.CharField(max_length=256, label='File name', required=True, 
                                widget=forms.TextInput(attrs={'readonly':'readonly'}))
-    content = forms.CharField(widget=forms.Textarea, label='File content')
+    content = MartorFormField(label='File content')
 
 
 class BranchSelector(forms.Form):
