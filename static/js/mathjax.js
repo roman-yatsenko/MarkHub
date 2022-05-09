898293f6e68402ea54ced0b92cb04b7910ddbf12
roman-yatsenko <yatsenkoroma@gmail.com>
2022-05-09 08:34:36 +0300

    Add MathJax processing for preview

---
+++ static/js/mathjax.js
@@ -0,0 +1,28 @@
+window.MathJax = {
+    tex: {
+      inlineMath: [["\\(", "\\)"]],
+      displayMath: [["\\[", "\\]"]],
+      processEscapes: true,
+      processEnvironments: true
+    },
+    options: {
+      ignoreHtmlClass: ".*|",
+      processHtmlClass: "arithmatex"
+    }
+  };
+  
+(function () {
+  var script = document.createElement('script');
+  script.src = 'https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-svg.js';
+  script.async = true;
+  document.head.appendChild(script);
+})();
+
+let preview = document.getElementById("nav-preview-tab-content");
+if (preview) {
+  preview.onclick = () => {
+    console.log('Preview clicked');
+    setTimeout(MathJax.typeset, 1000);
+    // MathJax.typeset();
+  }
+}
