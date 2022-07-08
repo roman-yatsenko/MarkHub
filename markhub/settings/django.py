275cd9cb2cc21794a2aab54931a30cbbc594214e
roman-yatsenko <yatsenkoroma@gmail.com>
2022-07-08 09:16:36 +0300

    Change Production secure policies

--- markhub/settings/django.py
+++ markhub/settings/django.py
@@ -150,13 +150,18 @@ STATIC_ROOT = BASE_DIR / "dist" / "static"
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-# HSTS policy & Referrer-Policy
+# Production secure policies (HSTS, Referrer-Policy, HTTPS)
 
 if not DEBUG:
+    SECURE_SSL_REDIRECT = True
+    SESSION_COOKIE_SECURE = True
+    CSRF_COOKIE_SECURE = True
+
     SECURE_HSTS_SECONDS = 2_592_000  # Unit is seconds; *USE A SMALL VALUE (30) FOR TESTING!*
     SECURE_HSTS_PRELOAD = True
     SECURE_HSTS_INCLUDE_SUBDOMAINS = True
     SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
+    
     SECURE_REFERRER_POLICY = "strict-origin-when-cross-origin"
     
 # Site admins
