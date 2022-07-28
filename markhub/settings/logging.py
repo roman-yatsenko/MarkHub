e0bbe1b688c8379d46689fac5a2d7ecb2f258965
roman-yatsenko <yatsenkoroma@gmail.com>
2022-07-28 07:12:28 +0300

    fix: error log

--- markhub/settings/logging.py
+++ markhub/settings/logging.py
@@ -4,10 +4,10 @@ from loguru import logger
 
 from .django import DEBUG
 
-ERROR_LOG_FILENAME = "logs/main.log"
+ERROR_LOG_FILENAME = "logs/errors.log"
 
 logger.remove()
-logger.add(ERROR_LOG_FILENAME, level="ERROR", rotation="00:00", retention="7 days")
+logger.add(ERROR_LOG_FILENAME, level="ERROR", retention="7 days")
 
 if DEBUG:
     logger.add(sys.stdout)
